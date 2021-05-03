99f524b8fb41ab0adccc019065fb837099835bb8
Behlül Şala <behlulsala@outlook.com>
2021-05-03 15:25:01 +0300

    rw

---
+++ DBConfig.php
@@ -0,0 +1,15 @@
+<?php
+ 
+//Define your host here.
+$HostName = "sql104.epizy.com";
+ 
+//Define your database name here.
+$DatabaseName = "epiz_28528907_koza";
+ 
+//Define your database username here.
+$HostUser = "epiz_28528907";
+ 
+//Define your database password here.
+$HostPass = "qpjM5udJQe";
+ 
+?>
